ab3e969f9922e14c2082485c13a3041111c8d6b7
Stu Hanford <shanford3@gmail.com>
2020-08-17 11:36:37 -0700

    Add basic outline of process

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -8,11 +8,11 @@ var htmlStrings = [
   '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
 ];
 
-describe('getElementsByClassName', function() {
+describe('getElementsByClassName', function () {
 
-  it('should match the results of calling the built-in function', function() {
+  it('should match the results of calling the built-in function', function () {
     $('body').addClass('targetClassName');
-    htmlStrings.forEach(function(htmlString) {
+    htmlStrings.forEach(function (htmlString) {
       var $rootElement = $(htmlString);
       $('body').append($rootElement);
 
@@ -20,7 +20,7 @@ describe('getElementsByClassName', function() {
       var expectedNodeList = document.getElementsByClassName('targetClassName');
       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
-      expect(equality).to.equal(FILL_ME_IN);
+      expect(equality).to.equal(true);
 
       $rootElement.remove();
     });
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function (className, current = document.body
 ) {
-  // your code here
+  var container = [];
+
+  //if thing has element w/ class, add to container
+  if (current.className === className) {
+    container.push(current);
+  }
+
+  //if thing doesn't have element w/ class, check for children
+  for (var i = 0; i < current.children.length; i++) {
+    var child = current.children[i];
+    getElementsByClassName(className, child);
+  }
+
+  console.log(container);
+  return container;
 };
